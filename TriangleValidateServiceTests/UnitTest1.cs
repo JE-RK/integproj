60756e0ac62f0ab72f0b58ee853fcfe1eb8fc23b
Dima <belomytsev78@gmail.com>
2023-03-30 23:49:07 +0300

    Добавьте файлы проекта.

---
+++ TriangleValidateServiceTests/UnitTest1.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using integtest;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Npgsql;
+using integtest.Interfaces;
+using integtest.Classes;
+using Microsoft.EntityFrameworkCore;
+
+namespace Laba4Tests
+{
+    [TestClass]
+    
+    public class TriangleValidateServiceUnitTests
+    {
+        private Mock<ITriangleProvider> triangleProvider;
+        private ITriangleService triangleService;
+        private ITriangleValidateService triangleValidateService;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            triangleProvider = new Mock<ITriangleProvider>();
+            triangleService = new TriangleService();
+            triangleValidateService = new TriangleValidateService(triangleProvider.Object, triangleService);
+        }
+        [TestMethod]
+        public void IsValid_True()
+        {
+            triangleProvider.Setup(m => m.GetById(It.IsAny<int>())).Returns(new Triangle(1, 7, 7, 7, 21.21762239271875, Triangle.TriangleType.Equilateral | Triangle.TriangleType.Oxygon));
+            Assert.AreEqual(true, triangleValidateService.IsValid(1));
+        }
+        [TestMethod]
+        public void IsValid_False()
+        {
+            triangleProvider.Setup(m => m.GetById(It.IsAny<int>())).Returns(new Triangle(2, -7, 7, 7, 0.0027712812921102037, Triangle.TriangleType.Equilateral | Triangle.TriangleType.Oxygon));
+            Assert.AreEqual(false, triangleValidateService.IsValid(2));
+        }
+        [TestMethod]
+        public void IsValid_EmptyTriangle()
+        {
+            triangleProvider.Setup(m => m.GetById(It.IsAny<int>())).Returns(new Triangle());
+            Assert.AreEqual(false, triangleValidateService.IsValid(4));
+
+        }
+        [TestMethod]
+        public void IsAllValid_True()
+        {
+            triangleProvider.Setup(m => m.GettAll()).Returns(new List<Triangle> { new Triangle(3, 7, 7, 7, 21.21762239271875, Triangle.TriangleType.Equilateral | Triangle.TriangleType.Oxygon), new Triangle(4, 7, 7, 7, 21.21762239271875, Triangle.TriangleType.Equilateral | Triangle.TriangleType.Oxygon) });
+            Assert.AreEqual(true, triangleValidateService.IsAllValid());
+        }
+        [TestMethod]
+        public void IsAllValid_False()
+        {
+            triangleProvider.Setup(m => m.GettAll()).Returns(new List<Triangle> { new Triangle(5, -7, 7, 7, 0.0027712812921102037, Triangle.TriangleType.Equilateral | Triangle.TriangleType.Oxygon), new Triangle(6, -7, 7, 7, 0.0027712812921102037, Triangle.TriangleType.Equilateral | Triangle.TriangleType.Oxygon) });
+            Assert.AreEqual(false, triangleValidateService.IsAllValid());
+
+        }
+        [TestMethod]
+        public void IsAllValid_EmptyTriangle()
+        {
+            triangleProvider.Setup(m => m.GettAll()).Returns(new List<Triangle> { new Triangle(), new Triangle() });
+            Assert.AreEqual(false, triangleValidateService.IsAllValid());
+        }
+    }
+    [TestClass]
+    public class TriangleValidateServiceIntegrationTests
+    {
+        private ITriangleProvider triangleProvider;
+        private ITriangleService triangleService;
+        private ApplicationContext applicationContext;
+        private ITriangleValidateService triangleValidateService;
+       
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            triangleProvider = new TriangleProvider();
+            triangleService = new TriangleService();
+            applicationContext = new ApplicationContext();
+            triangleValidateService = new TriangleValidateService(triangleProvider, triangleService, applicationContext);
+        }
+        [TestMethod]
+        public void IsValidTrue()
+        {
+            triangleProvider.Save(new Triangle(7, 9, 9, 9, 35.074028853269764, Triangle.TriangleType.Equilateral | Triangle.TriangleType.Oxygon));
+            bool actual = triangleValidateService.IsValid(7);
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void IsAllValid_false()
+        {
+            triangleProvider.Save(new Triangle(8, -9, 9, 9, 35.074028853269764, Triangle.TriangleType.Equilateral | Triangle.TriangleType.Oxygon));
+            bool actual = triangleValidateService.IsAllValid();
+            Assert.AreEqual(false, actual);
+        }
+        
+        [TestMethod]
+        public void GetTypeTriangle()
+        {
+            Triangle triangle;
+            triangleProvider.Save(triangle = new Triangle(9, 9, 9, 9, 35.074028853269764, Triangle.TriangleType.Equilateral | Triangle.TriangleType.Oxygon));
+            Triangle.TriangleType expected = triangle.type;
+            Triangle.TriangleType actual = triangleService.GetType(triangle.a, triangle.b, triangle.c);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void GetAreaTriangle()
+        {
+            Triangle triangle;
+            triangleProvider.Save(triangle = new Triangle(10, 9, 9, 9, 35.074028853269764, Triangle.TriangleType.Equilateral | Triangle.TriangleType.Oxygon));
+            double expected = triangle.area;
+            double actual = triangleService.GetArea(triangle.a, triangle.b, triangle.c);
+            Assert.IsTrue(Math.Abs(expected - actual) < 1e-9);
+        }
+        [TestMethod]
+        public void IsValidTriangle()
+        {
+            triangleProvider.Save(new Triangle(11, 10, 10, 1, 4.993746088859544, Triangle.TriangleType.Isosceles | Triangle.TriangleType.Oxygon));
+            Assert.IsTrue(triangleValidateService.IsValid(11));
+        }
+    }
+}
---
+++ integtest/Classes/TriangleValidateService.cs
@@ -0,0 +1,87 @@
+﻿using integtest.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace integtest.Classes
+{
+    public class TriangleValidateService : ITriangleValidateService
+    {
+        private readonly ITriangleProvider TriangleProvider;
+        private readonly ITriangleService TriangleService;
+        private readonly ApplicationContext db;
+
+        public TriangleValidateService(ITriangleProvider TriangleProvider, ITriangleService TriangleService, ApplicationContext db)
+        {
+            this.TriangleProvider = TriangleProvider;
+            this.TriangleService = TriangleService;
+            this.db = db;
+        }
+        public TriangleValidateService(ITriangleProvider TriangleProvider, ITriangleService TriangleService)
+        {
+            this.TriangleProvider = TriangleProvider;
+            this.TriangleService = TriangleService;
+        }
+        public bool IsAllValid()
+        {
+            
+            List<Triangle> list = TriangleProvider.GettAll();
+            bool b = true;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (TriangleService.IsValidTriangle(list[i].a, list[i].b, list[i].c) == false )
+                {
+                    list[i].isvalid = false;
+                    TriangleProvider.Update(list[i]);
+                    b = false;
+
+                }
+                if (TriangleService.GetType(list[i].a, list[i].b, list[i].c) != list[i].type)
+                {
+                    list[i].isvalid = false;
+                    TriangleProvider.Update(list[i]);
+                    b = false;
+                }
+                if (TriangleService.GetArea(list[i].a, list[i].b, list[i].c) != list[i].area)
+                {
+                    list[i].isvalid = false;
+                    TriangleProvider.Update(list[i]);
+                    b = false;
+                }
+                if (b == true)
+                {
+                    list[i].isvalid = true;
+                    TriangleProvider.Update(list[i]);
+                }
+            }
+            
+            return b;
+        }
+
+        public bool IsValid(int id)
+        {
+            Triangle triangle = TriangleProvider.GetById(id);
+            bool b = true;
+            if (TriangleService.IsValidTriangle(triangle.a, triangle.b, triangle.c) == false)
+            {
+                b = false;
+            }
+            else if (TriangleService.GetType(triangle.a, triangle.b, triangle.c) != (Triangle.TriangleType)triangle.type)
+            {
+                b = false;
+            }
+            else if (TriangleService.GetArea(triangle.a, triangle.b, triangle.c) != triangle.area)
+            {
+                b = false;
+            }
+            if(b == true)
+            {                              
+                triangle.isvalid = true;
+                TriangleProvider.Update(triangle);               
+            }
+            return b;
+        }
+    }
+}
